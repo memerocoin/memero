0cb4684f34380b0ebd03bf5d748283742d8dfe31
fuwa <fuwa25519@protonmail.com>
2020-10-04 21:50:55 +0800

    lol: init: set network id

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -207,10 +207,9 @@ namespace config
   uint16_t const RPC_DEFAULT_PORT = 34568;
   uint16_t const ZMQ_RPC_DEFAULT_PORT = 34569;
   boost::uuids::uuid const NETWORK_ID = { {
-      0x11, 0x33, 0xFF, 0x77 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
     } }; // Bender's nightmare
-  std::string const GENESIS_TX = "013c01ff0001ffffffffff1f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121012a1a936be5d91c01ee876e38c13fab0ee11cbe86011a2bf7740fb5ebd39d267d";
-  uint32_t const GENESIS_NONCE = 70;
+  std::string const GENESIS_TX = "";
+  uint32_t const GENESIS_NONCE = 42;
 
   // Hash domain separators
   const char HASH_KEY_BULLETPROOF_EXPONENT[] = "bulletproof";
@@ -237,10 +236,10 @@ namespace config
     uint16_t const RPC_DEFAULT_PORT = 11181;
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 11182;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x11, 0x33, 0xFF, 0x77 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
+        0x10, 0x42, 0xEE, 0x66, 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
       } }; // Bender's daydream
     std::string const GENESIS_TX = "013c01ff0001ffffffffff1f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210160eb755f618a2336055dee60f307fe0ded81c5b37b53d310175ca9ee69b0c8ad";
-    uint32_t const GENESIS_NONCE = 70;
+    uint32_t const GENESIS_NONCE = 42;
   }
 
   namespace stagenet
@@ -301,6 +300,7 @@ namespace cryptonote
       ::config::testnet::P2P_DEFAULT_PORT,
       ::config::testnet::RPC_DEFAULT_PORT,
       ::config::testnet::ZMQ_RPC_DEFAULT_PORT,
+
       ::config::testnet::NETWORK_ID,
       ::config::testnet::GENESIS_TX,
       ::config::testnet::GENESIS_NONCE
