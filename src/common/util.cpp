0c275ab3e2daabc912b0b0b8112be7a09b199f00
fuwa <fuwa25519@protonmail.com>
2020-10-05 22:14:11 +0800

    lol: light: remove unbound

--- src/common/util.cpp
+++ src/common/util.cpp
@@ -54,7 +54,6 @@
   #include <fstream>
 #endif
 
-#include "unbound.h"
 
 #include "include_base_utils.h"
 #include "file_io_utils.h"
@@ -672,6 +671,7 @@ std::string get_nix_version_display_string()
     return std::error_code(code, std::system_category());
   }
 
+  /*
   static bool unbound_built_with_threads()
   {
     ub_ctx *ctx = ub_ctx_create();
@@ -686,6 +686,7 @@ std::string get_nix_version_display_string()
     MINFO("libunbound was built " << (with_threads ? "with" : "without") << " threads");
     return with_threads;
   }
+  */
 
   bool sanitize_locale()
   {
@@ -794,9 +795,6 @@ std::string get_nix_version_display_string()
 #else
     OPENSSL_init_ssl(0, NULL);
 #endif
-
-    if (!unbound_built_with_threads())
-      MCLOG_RED(el::Level::Warning, "global", "libunbound was not built with threads enabled - crashes may occur");
 
     return true;
   }
