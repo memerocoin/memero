966283b8d59410d7d73ac9c73df43397f765a810
fuwa <fuwa25519@protonmail.com>
2020-10-09 17:09:01 +0800

    lol: init: add genesis tx

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -208,9 +208,19 @@ namespace config
   uint16_t const RPC_DEFAULT_PORT = 45679;
   uint16_t const ZMQ_RPC_DEFAULT_PORT = 45680;
   boost::uuids::uuid const NETWORK_ID = { {
+      0x69, 0x42, 0xEE, 0x66 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
     } }; // Bender's nightmare
-  std::string const GENESIS_TX = "";
-  uint32_t const GENESIS_NONCE = 42;
+  std::string const GENESIS_TX = "013c01ff0001ffffffffff1f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121012a1a936be5d91c01ee876e38c13fab0ee11cbe86011a2bf7740fb5ebd39d267d";
+
+  // twitter trending Oct 10th, 2020 20:08 (UTC)
+
+  /*
+  echo -n "Texas The No. 22-ranked Texas Longhorns take on the 1-2 Oklahoma Sooners in the Red River Showdown" | sha256sum
+
+  b818dfa125b74ae5a09e1d4c56477b27989a08d17a6dafaa55b8c26e67111aa0  -
+  */
+
+  uint32_t const GENESIS_NONCE = 0xb818dfa1;
 
   // Hash domain separators
   const char HASH_KEY_BULLETPROOF_EXPONENT[] = "bulletproof";
