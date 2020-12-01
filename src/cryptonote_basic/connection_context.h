964e26055d80b0ca06cd7f5d1cf7238a74b2e791
moneromooo <moneromoo@nowhere.nowhere.nowhere>
2020-12-01 22:25:07 +0000

    protocol: add scoring system to drop peers that don't behave

--- src/cryptonote_basic/connection_context.h
+++ src/cryptonote_basic/connection_context.h
@@ -43,7 +43,7 @@ namespace cryptonote
   {
     cryptonote_connection_context(): m_state(state_before_handshake), m_remote_blockchain_height(0), m_last_response_height(0),
         m_last_request_time(boost::date_time::not_a_date_time), m_callback_request_count(0),
-        m_last_known_hash(crypto::null_hash), m_pruning_seed(0), m_anchor(false) {}
+        m_last_known_hash(crypto::null_hash), m_pruning_seed(0), m_anchor(false), m_score(0) {}
 
     enum state
     {
@@ -64,7 +64,7 @@ namespace cryptonote
     crypto::hash m_last_known_hash;
     uint32_t m_pruning_seed;
     bool m_anchor;
-    //size_t m_score;  TODO: add score calculations
+    int32_t m_score;
   };
 
   inline std::string get_protocol_state_string(cryptonote_connection_context::state s)
