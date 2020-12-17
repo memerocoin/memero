2280ded6ffd4ee87628595a25f6e1e988ddc9379
fuwa <fuwa25519@protonmail.com>
2020-12-17 08:37:58 +0800

    Revert "Fixed bugs for take_slice and byte_stream->byte_slice"
    This reverts commit 98c987396079a6ac71a8777e331e8aafd9ed99a3.

--- contrib/epee/src/byte_slice.cpp
+++ contrib/epee/src/byte_slice.cpp
@@ -176,14 +176,9 @@ namespace epee
   byte_slice::byte_slice(byte_stream&& stream) noexcept
     : storage_(nullptr), portion_(stream.data(), stream.size())
   {
-    if (stream.size())
-    {
-      std::uint8_t* const data = stream.take_buffer().release() - sizeof(raw_byte_slice);
-      new (data) raw_byte_slice{};
-      storage_.reset(reinterpret_cast<raw_byte_slice*>(data));
-    }
-    else
-      portion_ = nullptr;
+    std::uint8_t* const data = stream.take_buffer().release() - sizeof(raw_byte_slice);
+    new (data) raw_byte_slice{};
+    storage_.reset(reinterpret_cast<raw_byte_slice*>(data));
   }
 
   byte_slice::byte_slice(byte_slice&& source) noexcept
@@ -213,17 +208,14 @@ namespace epee
   byte_slice byte_slice::take_slice(const std::size_t max_bytes) noexcept
   {
     byte_slice out{};
+    std::uint8_t const* const ptr = data();
+    out.portion_ = {ptr, portion_.remove_prefix(max_bytes)};
 
-    if (max_bytes)
-    {
-      std::uint8_t const* const ptr = data();
-      out.portion_ = {ptr, portion_.remove_prefix(max_bytes)};
+    if (portion_.empty())
+      out.storage_ = std::move(storage_); // no atomic inc/dec
+    else
+      out = {storage_.get(), out.portion_};
 
-      if (portion_.empty())
-        out.storage_ = std::move(storage_); // no atomic inc/dec
-      else
-        out = {storage_.get(), out.portion_};
-    }
     return out;
   }
 
