a8b8171acf09693c24d10129ea7efc788961de2c
fuwa <fuwa25519@protonmail.com>
2020-12-26 13:32:42 +0800

    lol: ui: do not ask password by default

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -1049,7 +1049,7 @@ wallet2::wallet2(network_type nettype, uint64_t kdf_rounds, bool unattended, std
   m_refresh_from_block_height(0),
   m_explicit_refresh_from_block_height(true),
   m_confirm_non_default_ring_size(true),
-  m_ask_password(AskPasswordOnAction),
+  m_ask_password(AskPasswordNever),
   m_min_output_count(0),
   m_min_output_value(0),
   m_merge_destinations(false),
