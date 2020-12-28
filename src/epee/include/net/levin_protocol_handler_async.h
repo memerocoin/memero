61e46d5c7ef3045568387a4e8771593b7f5bf336
fuwa <fuwa25519@protonmail.com>
2020-12-28 22:39:08 +0800

    lol: dep: boost::unordered_map -> std::unordered_map

--- src/epee/include/net/levin_protocol_handler_async.h
+++ src/epee/include/net/levin_protocol_handler_async.h
@@ -27,7 +27,7 @@
 #pragma once
 #include <boost/asio/deadline_timer.hpp>
 #include <boost/uuid/uuid_generators.hpp>
-#include <boost/unordered_map.hpp>
+#include <unordered_map>
 #include <boost/interprocess/detail/atomic.hpp>
 
 #include <atomic>
@@ -64,7 +64,7 @@ class async_protocol_handler;
 template<class t_connection_context>
 class async_protocol_handler_config
 {
-  typedef boost::unordered_map<boost::uuids::uuid, async_protocol_handler<t_connection_context>* > connections_map;
+  typedef std::unordered_map<boost::uuids::uuid, async_protocol_handler<t_connection_context>*, boost::hash<boost::uuids::uuid>> connections_map;
   critical_section m_connects_lock;
   connections_map m_connects;
 
