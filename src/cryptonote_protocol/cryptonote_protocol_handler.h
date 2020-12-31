7fd2866a16d41f68efd58b553b730bc5a797b329
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2020-12-31 16:51:59 +0000

    add a max levin packet size by command type

--- src/cryptonote_protocol/cryptonote_protocol_handler.h
+++ src/cryptonote_protocol/cryptonote_protocol_handler.h
@@ -109,6 +109,7 @@ namespace cryptonote
     std::list<connection_info> get_connections();
     const block_queue &get_block_queue() const { return m_block_queue; }
     void stop();
+    void on_connection_new(cryptonote_connection_context &context);
     void on_connection_close(cryptonote_connection_context &context);
     void set_max_out_peers(unsigned int max) { m_max_out_peers = max; }
     std::string get_peers_overview() const;
