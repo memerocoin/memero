579393891401d276ed0633b18c0ca5294ec34de5
fuwa <fuwa25519@protonmail.com>
2021-02-10 01:46:32 +0800

    lol: simplewallet: strip accept_loaded_tx

--- src/wallet/cli/simplewallet.h
+++ src/wallet/cli/simplewallet.h
@@ -207,9 +207,6 @@ namespace cryptonote
     uint64_t get_daemon_blockchain_height(std::string& err);
     bool try_connect_to_daemon(bool silent = false, uint32_t* version = nullptr);
     bool ask_wallet_create_if_needed();
-    bool accept_loaded_tx(const std::function<size_t()> get_num_txes, const std::function<const tools::wallet2::tx_construction_data&(size_t)> &get_tx, const std::string &extra_message = std::string());
-    bool accept_loaded_tx(const tools::wallet2::unsigned_tx_set &txs);
-    bool accept_loaded_tx(const tools::wallet2::signed_tx_set &txs);
     bool process_ring_members(const std::vector<tools::wallet2::pending_tx>& ptx_vector, std::ostream& ostr, bool verbose);
     std::string get_prompt() const;
     bool print_seed(bool encrypted);
