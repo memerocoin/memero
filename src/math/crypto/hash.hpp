b3b072a9c116b0adbfcae283dc9373d1ecc6a7fb
fuwa <fuwa25519@protonmail.com>
2021-03-05 03:22:33 +0800

    lol: strip: pod class

--- src/math/crypto/hash.hpp
+++ src/math/crypto/hash.hpp
@@ -33,7 +33,6 @@
 #include <stddef.h>
 #include <iostream>
 
-#include "common/pod-class.h"
 #include "generic-ops.h"
 #include "hex.h"
 #include "sha3.hpp"
@@ -43,10 +42,10 @@
 namespace crypto {
 
 #pragma pack(push, 1)
-  POD_CLASS hash {
+  struct hash {
     char data[HASH_SIZE];
   };
-  POD_CLASS hash8 {
+  struct hash8 {
     char data[8];
   };
 #pragma pack(pop)
