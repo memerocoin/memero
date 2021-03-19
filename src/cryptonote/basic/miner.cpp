1fda88f9aa582229d6d0eafc39cd8e53cd8d8cb5
fuwa <fuwa25519@protonmail.com>
2021-03-19 14:12:40 +0800

    lol: daemon: remove extra-message-file

--- src/cryptonote/basic/miner.cpp
+++ src/cryptonote/basic/miner.cpp
@@ -88,7 +88,6 @@ namespace cryptonote
 
   namespace
   {
-    const command_line::arg_descriptor<std::string> arg_extra_messages =  {"extra-messages-file", "Specify file for extra messages to include into coinbase transactions", "", true};
     const command_line::arg_descriptor<std::string> arg_start_mining =    {"start-mining", "Specify wallet address to mining for", "", true};
     const command_line::arg_descriptor<uint32_t>      arg_mining_threads =  {"mining-threads", "Specify mining threads count", 0, true};
   }
@@ -204,7 +203,6 @@ namespace cryptonote
   //-----------------------------------------------------------------------------------------------------
   void miner::init_options(boost::program_options::options_description& desc)
   {
-    command_line::add_arg(desc, arg_extra_messages);
     command_line::add_arg(desc, arg_start_mining);
     command_line::add_arg(desc, arg_mining_threads);
   }
