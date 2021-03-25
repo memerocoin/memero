2e74faeba63b609b089348b96d8bf09839caac6a
fuwa <fuwa25519@protonmail.com>
2021-03-25 18:16:39 +0800

    lol: nix: add testing option in pkg

--- etc/nix/lolnero.nix
+++ etc/nix/lolnero.nix
@@ -1,20 +1,21 @@
 { gcc10Stdenv
 , cmake, git, fetchgit
 , boost175, openssl, readline, libsodium, rapidjson
-, lib
+, lib, gmock
 }:
 
 let
 
   stdenv = gcc10Stdenv
-; lolnero-rev = "v0.9.0.1"
-; lolnero-sha256 = "1r58753zcacjlaq0h86xx837rsnq5g0mhyvkg2p6nkbmpiacy0dk"
+; lolnero-rev = "v0.9.1.0"
+; lolnero-sha256 = "1qahvswfqwfp6lpmxhrh8jhbp47nlfvnny6s3gdqhlrd8h4pxfrc"
+; doCheck = true
 
 ; in
 
 stdenv.mkDerivation rec {
   pname = "lolnero";
-  version = "0.9.0.1";
+  version = "0.9.1.0";
   src = fetchgit {
     url = "https://gitlab.com/fuwa/lolnero.git";
     rev = lolnero-rev;
@@ -24,15 +25,22 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ cmake ];
 
+  inherit doCheck;
+
   buildInputs = [
-    boost175 openssl readline libsodium rapidjson
-  ];
+    boost175 openssl readline libsodium rapidjson gmock
+  ]
+  ++ lib.optionals doCheck [gmock]
+  ;
+
 
   cmakeFlags = [
     "--no-warn-unused-cli"
     "-DReadline_ROOT_DIR=${readline.dev}"
     "-DVERSIONTAG=${lolnero-rev}"
-  ];
+  ]
+  ++ lib.optionals doCheck ["-DBUILD_TESTING=ON"]
+  ;
 
   meta = with lib; {
     description = "A fork of Wownero with a linear emission and a SHA-3 PoW";
