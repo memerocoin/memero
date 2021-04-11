3ba4a710dff427e20a71ec3a93cafeb41b0154f1
fuwa <fuwa25519@protonmail.com>
2021-04-11 08:03:44 +0800

    lol: ringct: use sodium.h

--- src/math/ringct/rctOps.cpp
+++ src/math/ringct/rctOps.cpp
@@ -29,10 +29,13 @@
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <boost/lexical_cast.hpp>
-#include "tools/epee/include/misc_log_ex.h"
+
+#include <sodium.h>
+
 #include "cryptonote/basic/cryptonote_format_utils.h"
-#include "rctOps.hpp"
 #include "curveConstants.hpp"
+#include "rctOps.hpp"
+#include "tools/epee/include/misc_log_ex.h"
 #include "zeroCommitment.hpp"
 
 using namespace crypto;
