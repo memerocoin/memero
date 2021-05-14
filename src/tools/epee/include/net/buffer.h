bfe20009a9780c45ca6ceb749e4be2f60da0f458
fuwa <fuwa25519@protonmail.com>
2021-05-14 13:11:58 +0800

    lol: epee: simplify buffer

--- src/tools/epee/include/net/buffer.h
+++ src/tools/epee/include/net/buffer.h
@@ -48,14 +48,14 @@ public:
   buffer(size_t reserve = 0): offset(0) { storage.reserve(reserve); }
 
   void append(const void *data, size_t sz);
-  void erase(size_t sz) { NET_BUFFER_LOG("erasing " << sz << "/" << size()); CHECK_AND_ASSERT_THROW_MES(offset + sz <= storage.size(), "erase: sz too large"); offset += sz; if (offset == storage.size()) { storage.resize(0); offset = 0; } }
-  epee::span<const uint8_t> span(size_t sz) const { CHECK_AND_ASSERT_THROW_MES(sz <= size(), "span is too large"); return epee::span<const uint8_t>(storage.data() + offset, sz); }
+  void erase(size_t sz);
+  epee::span<const uint8_t> span(size_t sz) const;
   // carve must keep the data in scope till next call, other API calls (such as append, erase) can invalidate the carved buffer
-  epee::span<const uint8_t> carve(size_t sz) { CHECK_AND_ASSERT_THROW_MES(sz <= size(), "span is too large"); offset += sz; return epee::span<const uint8_t>(storage.data() + offset - sz, sz); }
-  size_t size() const { return storage.size() - offset; }
+  epee::span<const uint8_t> carve(size_t sz);
+  size_t size() const;
 
 private:
-  std::vector<uint8_t> storage;
+  std::basic_string<uint8_t> storage;
   size_t offset;
 };
 }
--- src/tools/epee/src/buffer.cpp
+++ src/tools/epee/src/buffer.cpp
@@ -37,62 +37,34 @@ namespace epee
 namespace net_utils
 {
 
-void buffer::append(const void *data, size_t sz)
-{
-  const size_t capacity = storage.capacity();
-  const size_t avail = capacity - storage.size();
-
-  CHECK_AND_ASSERT_THROW_MES(storage.size() < std::numeric_limits<size_t>::max() - sz, "Too much data to append");
-
-  // decide when to move
-  if (sz > avail)
-  {
-    // we have to reallocate or move
-    const bool move = size() + sz <= capacity;
-    if (move)
-    {
-      const size_t bytes = storage.size() - offset;
-      NET_BUFFER_LOG("appending " << sz << " from " << size() << " by moving " << bytes << " from offset " << offset << " first (forced)");
-      memmove(storage.data(), storage.data() + offset, bytes);
-      storage.resize(bytes);
-      offset = 0;
-    }
-    else
-    {
-      NET_BUFFER_LOG("appending " << sz << " from " << size() << " by reallocating");
-      std::vector<uint8_t> new_storage;
-      size_t reserve = (((size() + sz) * 3 / 2) + 4095) & ~4095;
-      new_storage.reserve(reserve);
-      new_storage.resize(size());
-      if (size() > 0)
-        memcpy(new_storage.data(), storage.data() + offset, storage.size() - offset);
-      offset = 0;
-      std::swap(storage, new_storage);
-    }
+  void buffer::append(const void *data, size_t sz) {
+    storage.append((uint8_t*)data, sz);
   }
-  else
-  {
-    // we have space already
-    if (size() <= 4096 && offset > 4096 * 16 && offset >= capacity / 2)
-    {
-      // we have little to move, and we're far enough into the buffer that it's probably a win to move anyway
-      const size_t pos = storage.size() - offset;
-      NET_BUFFER_LOG("appending " << sz << " from " << size() << " by moving " << pos << " from offset " << offset << " first (unforced)");
-      memmove(storage.data(), storage.data() + offset, storage.size() - offset);
-      storage.resize(pos);
+
+  void buffer::erase(size_t sz) {
+    NET_BUFFER_LOG("erasing " << sz << "/" << size());
+    CHECK_AND_ASSERT_THROW_MES(offset + sz <= storage.size(), "erase: sz too large");
+    offset += sz;
+    if (offset == storage.size()) {
+      storage.clear();
       offset = 0;
-    }
-    else
-    {
-      NET_BUFFER_LOG("appending " << sz << " from " << size() << " by writing to existing capacity");
     }
   }
 
-  // add the new data
-  storage.insert(storage.end(), (const uint8_t*)data, (const uint8_t*)data + sz);
+  epee::span<const uint8_t> buffer::span(size_t sz) const {
+    CHECK_AND_ASSERT_THROW_MES(sz <= size(), "span is too large");
+    return epee::span<const uint8_t>(storage.data() + offset, sz);
+  }
 
-  NET_BUFFER_LOG("storage now " << offset << "/" << storage.size() << "/" << storage.capacity());
-}
+  epee::span<const uint8_t> buffer::carve(size_t sz) {
+    CHECK_AND_ASSERT_THROW_MES(sz <= size(), "span is too large");
+    offset += sz;
+    return epee::span<const uint8_t>(storage.data() + offset - sz, sz);
+  }
+
+  size_t buffer::size() const {
+    return storage.size() - offset;
+  }
 
 }
 }
