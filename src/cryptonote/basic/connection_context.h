84ec83f559cbc5969f98976820d6f15a4b2f86c6
fuwa <fuwa25519@protonmail.com>
2021-05-20 23:32:28 +0800

    lol: cn/basic: add connection_context.cpp

--- src/cryptonote/basic/connection_context.h
+++ src/cryptonote/basic/connection_context.h
@@ -1,21 +1,21 @@
 // Copyright (c) 2014-2020, The Monero Project
-// 
+//
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice, this list of
 //    conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice, this list
 //    of conditions and the following disclaimer in the documentation and/or other
 //    materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its contributors may be
 //    used to endorse or promote products derived from this software without specific
 //    prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
@@ -25,10 +25,11 @@
 // INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
 
 #pragma once
+
 #include <unordered_set>
 #include <atomic>
 #include <algorithm>
@@ -54,28 +55,10 @@ namespace cryptonote
       state_normal
     };
 
-    bool handshake_complete() const noexcept { return m_state != state_before_handshake; }
+    bool handshake_complete() const noexcept;
 
-    void set_max_bytes(int command, size_t bytes) {
-      const auto i = std::lower_bound(m_max_bytes.begin(), m_max_bytes.end(), std::make_pair(command, bytes), [](const std::pair<int, size_t> &e0, const std::pair<int, size_t> &e1){
-        return e0.first < e1.first;
-      });
-      if (i == m_max_bytes.end())
-        m_max_bytes.push_back(std::make_pair(command, bytes));
-      else if (i->first == command)
-        i->second = bytes;
-      else
-        m_max_bytes.insert(i, std::make_pair(command, bytes));
-    }
-    size_t get_max_bytes(int command) const {
-      const auto i = std::lower_bound(m_max_bytes.begin(), m_max_bytes.end(), std::make_pair(command, 0), [](const std::pair<int, size_t> &e0, const std::pair<int, size_t> &e1){
-        return e0.first < e1.first;
-      });
-      if (i == m_max_bytes.end() || i->first != command)
-        return std::numeric_limits<size_t>::max();
-      else
-        return i->second;
-    }
+    void set_max_bytes(const int command, const size_t bytes);
+    size_t get_max_bytes(const int command) const;
 
     state m_state;
     std::vector<std::pair<crypto::hash, uint64_t>> m_needed_objects;
@@ -94,42 +77,6 @@ namespace cryptonote
     std::vector<std::pair<int, size_t>> m_max_bytes;
   };
 
-  inline std::string get_protocol_state_string(cryptonote_connection_context::state s)
-  {
-    switch (s)
-    {
-    case cryptonote_connection_context::state_before_handshake:
-      return "before_handshake";
-    case cryptonote_connection_context::state_synchronizing:
-      return "synchronizing";
-    case cryptonote_connection_context::state_standby:
-      return "standby";
-    case cryptonote_connection_context::state_idle:
-      return "idle";
-    case cryptonote_connection_context::state_normal:
-      return "normal";
-    default:
-      return "unknown";
-    }    
-  }
-
-  inline char get_protocol_state_char(cryptonote_connection_context::state s)
-  {
-    switch (s)
-    {
-    case cryptonote_connection_context::state_before_handshake:
-      return 'h';
-    case cryptonote_connection_context::state_synchronizing:
-      return 's';
-    case cryptonote_connection_context::state_standby:
-      return 'w';
-    case cryptonote_connection_context::state_idle:
-      return 'i';
-    case cryptonote_connection_context::state_normal:
-      return 'n';
-    default:
-      return 'u';
-    }
-  }
-
+  std::string get_protocol_state_string(const cryptonote_connection_context::state s);
+  char get_protocol_state_char(const cryptonote_connection_context::state s);
 }
