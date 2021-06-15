97720e7f03b4f42e4fb91a0d25acd1ff4a4f93d9
fuwa <fuwa25519@protonmail.com>
2021-06-15 21:29:22 +0800

    lol: miner: remove autodetect arg

--- src/daemon/command_server.cpp
+++ src/daemon/command_server.cpp
@@ -110,7 +110,7 @@ t_command_server::t_command_server(
   m_command_lookup.set_handler(
       "start_mining"
     , std::bind(&t_command_parser_executor::start_mining, &m_parser, p::_1)
-    , "start_mining <addr> [<threads>|auto]"
+    , "start_mining <addr> [<threads>]"
     , "Start mining for specified address. Defaults to 1 thread and no background mining. Use \"auto\" to autodetect optimal number of threads."
     );
   m_command_lookup.set_handler(
