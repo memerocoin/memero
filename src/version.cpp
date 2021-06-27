474c630f1cc53a89ff5090c2e3720eae8aecc683
fuwa <fuwa25519@protonmail.com>
2021-06-27 17:26:58 +0800

    lol: release: v0.9.7.12

--- src/version.cpp
+++ src/version.cpp
@@ -1,4 +1,4 @@
 #include "config/version.hpp"
 
-constexpr std::string_view LOLNERO_VERSION = "0.9.7.11";
+constexpr std::string_view LOLNERO_VERSION = "0.9.7.12";
 constexpr std::string_view LOLNERO_RELEASE_NAME = "Saber";
