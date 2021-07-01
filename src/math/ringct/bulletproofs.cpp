366f54487f1f9c09d4ec1d5ceae998526d432b62
fuwa <fuwa25519@protonmail.com>
2021-07-02 05:17:41 +0800

    lol: math/bp: de-optimize bulletproof_MAKE since it's not a bottleneck

--- src/math/ringct/bulletproofs.cpp
+++ src/math/ringct/bulletproofs.cpp
@@ -452,16 +452,16 @@ rct::key hash_cache_mash(rct::key &hash_cache, const rct::key &mash0, const rct:
 /* Given a value v (0..2^N-1) and a mask gamma, construct a range proof */
 Bulletproof bulletproof_MAKE(const rct::key sv, const rct::key gamma)
 {
-  return bulletproof_MAKE(std::array<rct::key, 1>{sv}, rct::keyV(1, gamma));
+  return bulletproof_MAKE(std::vector<rct::key>{sv}, rct::keyV(1, gamma));
 }
 
 Bulletproof bulletproof_MAKE(const uint64_t v, const rct::key gamma)
 {
-  return bulletproof_MAKE(std::array<uint64_t, 1>{v}, rct::keyV(1, gamma));
+  return bulletproof_MAKE(std::vector<uint64_t>{v}, rct::keyV(1, gamma));
 }
 
 /* Given a set of values v (0..2^N-1) and masks gamma, construct a range proof */
-Bulletproof bulletproof_MAKE(const std::span<const rct::key> sv, const rct::keyV gamma)
+Bulletproof bulletproof_MAKE(const rct::keyV sv, const rct::keyV gamma)
 {
   ASSERT_OR_LOG_THROW(sv.size() == gamma.size(), "Incompatible sizes of sv and gamma");
   ASSERT_OR_LOG_THROW(!sv.empty(), "sv is empty");
@@ -694,7 +694,7 @@ try_again:
   return Bulletproof(std::move(V), A, S, T1, T2, taux, mu, std::move(L), std::move(R), aprime[0], bprime[0], t);
 }
 
-Bulletproof bulletproof_MAKE(const std::span<const uint64_t> v, const rct::keyV gamma)
+Bulletproof bulletproof_MAKE(const std::vector<uint64_t> v, const rct::keyV gamma)
 {
   ASSERT_OR_LOG_THROW(v.size() == gamma.size(), "Incompatible sizes of v and gamma");
 
