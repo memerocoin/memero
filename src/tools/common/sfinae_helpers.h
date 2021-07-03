f32b10a008bbc9991915b1dbfca97936be84c3e1
fuwa <fuwa25519@protonmail.com>
2021-07-04 00:09:38 +0800

    lol: tools: shorten sfinae helpers ...

--- src/tools/common/sfinae_helpers.h
+++ src/tools/common/sfinae_helpers.h
@@ -41,42 +41,6 @@ namespace sfinae
   struct false_type { true_type a[2]; };
 
   template <typename T>
-  struct is_not_container
-  {
-  private:
-
-    // does not have const iterator
-    template <typename C> static false_type c_iter(typename C::const_iterator*);
-    template <typename C> static true_type c_iter(...);
-
-    // does not have value_type
-    template <typename C> static false_type v_type(typename C::value_type*);
-    template <typename C> static true_type v_type(...);
-
-    // does not have key_type
-    template <typename C> static false_type k_type(typename C::key_type*);
-    template <typename C> static true_type k_type(...);
-
-    // does not have mapped_type
-    template <typename C> static false_type m_type(typename C::mapped_type*);
-    template <typename C> static true_type m_type(...);
-
-  public:
-
-    static const bool value = (
-      (
-        sizeof(c_iter<T>(0)) == sizeof(true_type) &&
-        sizeof(v_type<T>(0)) == sizeof(true_type) &&
-        sizeof(k_type<T>(0)) == sizeof(true_type) &&
-        sizeof(m_type<T>(0)) == sizeof(true_type)
-      )
-      || std::is_same<T, std::string>::value
-    );
-
-    typedef T type;
-  };
-
-  template <typename T>
   struct is_vector_like
   {
   private:
@@ -85,60 +49,10 @@ namespace sfinae
     template <typename C> static true_type c_iter(typename C::const_iterator*);
     template <typename C> static false_type c_iter(...);
 
-    // has value_type
-    template <typename C> static true_type v_type(typename C::value_type*);
-    template <typename C> static false_type v_type(...);
-
-    // does not have key_type
-    template <typename C> static false_type k_type(typename C::key_type*);
-    template <typename C> static true_type k_type(...);
-
-    // does not have mapped_type
-    template <typename C> static false_type m_type(typename C::mapped_type*);
-    template <typename C> static true_type m_type(...);
-
   public:
 
     static const bool value = (
-        sizeof(c_iter<T>(0)) == sizeof(true_type) &&
-        sizeof(v_type<T>(0)) == sizeof(true_type) &&
-        sizeof(k_type<T>(0)) == sizeof(true_type) &&
-        sizeof(m_type<T>(0)) == sizeof(true_type) &&
-        !std::is_same<T, std::string>::value
-    );
-
-    typedef T type;
-  };
-
-  template <typename T>
-  struct is_map_like
-  {
-  private:
-
-    // has const iterator
-    template <typename C> static true_type c_iter(typename C::const_iterator*);
-    template <typename C> static false_type c_iter(...);
-
-    // has value_type
-    template <typename C> static true_type v_type(typename C::value_type*);
-    template <typename C> static false_type v_type(...);
-
-    // has key_type
-    template <typename C> static true_type k_type(typename C::key_type*);
-    template <typename C> static false_type k_type(...);
-
-    // has mapped_type
-    template <typename C> static true_type m_type(typename C::mapped_type*);
-    template <typename C> static false_type m_type(...);
-
-  public:
-
-    static const bool value = (
-        sizeof(c_iter<T>(0)) == sizeof(true_type) &&
-        sizeof(v_type<T>(0)) == sizeof(true_type) &&
-        sizeof(k_type<T>(0)) == sizeof(true_type) &&
-        sizeof(m_type<T>(0)) == sizeof(true_type) &&
-        !std::is_same<T, std::string>::value
+        sizeof(c_iter<T>(0)) == sizeof(true_type)
     );
 
     typedef T type;
