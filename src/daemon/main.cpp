860fd6d2bf8a1a22e50767f50aa20a6e837fbc1d
fuwa <fuwa25519@protonmail.com>
2021-07-15 19:14:12 +0800

    lol: cmake: remove spdlog

--- src/daemon/main.cpp
+++ src/daemon/main.cpp
@@ -109,9 +109,6 @@ int main(int argc, char const * argv[])
     // Monero Version
     if (command_line::get_arg(vm, command_line::arg_version))
     {
-      // spdlog::info("Welcome to spdlog!");
-      // log->info("loggers can be retrieved from a global registry using the spdlog::get(logger_name)");
-
       std::cout << "Lolnero '" << LOLNERO_RELEASE_NAME << "' (v" << LOLNERO_VERSION_FULL << ")" << ENDL;
       return 0;
     }
