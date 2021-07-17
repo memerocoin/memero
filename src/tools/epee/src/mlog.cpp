52f3d59111c36973f0e97b7198515862643ab293
fuwa <fuwa25519@protonmail.com>
2021-07-17 23:17:05 +0800

    lol: logging: clean up spdlog

--- src/tools/epee/src/mlog.cpp
+++ src/tools/epee/src/mlog.cpp
@@ -29,9 +29,6 @@
 
 #include "config/lol.hpp"
 
-// #include "spdlog/spdlog.h"
-// #include "spdlog/sinks/stdout_color_sinks.h"
-
 #include <filesystem>
 #include <set>
 #include <atomic>
@@ -281,61 +278,32 @@ void reset_console_color() {
 
 }
 
-// SPDLOG_LEVEL_TRACE,
-// SPDLOG_LEVEL_DEBUG,
-// SPDLOG_LEVEL_INFO,
-// SPDLOG_LEVEL_WARN,
-// SPDLOG_LEVEL_ERROR,
-// SPDLOG_LEVEL_CRITICAL,
-// SPDLOG_LEVEL_OFF
-
 const std::set<std::string> default_cat = {"global", "logging"};
 
 void log_level_map(const el::Level level, const std::string cat, const std::string_view x) {
-  // auto _spd_log_handle = spdlog::get(cat);
-
-  // if (!_spd_log_handle) {
-  //   _spd_log_handle = spdlog::stdout_color_mt(cat);
-  //   std::string format_str;
-  //   if (default_cat.find(cat) != default_cat.end()) {
-  //     format_str = "%Y-%m-%d %T.%e %L %v";
-  //   } else {
-  //     format_str = "%Y-%m-%d %T.%e %L [%n] %v";
-  //   }
-  //   _spd_log_handle->set_pattern(format_str, spdlog::pattern_time_type::utc);
-  // }
-
-
   const std::string cat_str = default_cat.find(cat) != default_cat.end() ? "" : "[" + cat + "] ";
 
   std::string log_header;
   switch (level) {
   case el::Level::Trace:
-    // _spd_log_handle->trace(x);
     log_header = "T";
     break;
   case el::Level::Debug:
-    // _spd_log_handle->debug(x);
     log_header = "D";
     break;
   case el::Level::Info:
-    // _spd_log_handle->info(x);
     log_header = "I";
     break;
   case el::Level::Warning:
-    // _spd_log_handle->warn(x);
     log_header = "W";
     break;
   case el::Level::Error:
-    // _spd_log_handle->error(x);
     log_header = "E";
     break;
   case el::Level::Fatal:
-    // _spd_log_handle->critical(x);
     log_header = "F";
     break;
   default:
-    // _spd_log_handle->off(x);
     break;
   }
 
