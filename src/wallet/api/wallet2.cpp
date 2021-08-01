4416aa6f53b0e837301b65ad8c1535205c031a81
fuwa <fuwa25519@protonmail.com>
2021-08-02 07:10:43 +0800

    lol: wallet/api: use verbose for printing

--- src/wallet/api/wallet2.cpp
+++ src/wallet/api/wallet2.cpp
@@ -987,7 +987,7 @@ void wallet2::process_new_transaction(const crypto::hash &txid, const cryptonote
             if (td.m_key_image_known)
 	      m_key_images[td.m_key_image] = m_transfers.size()-1;
 	    m_pub_keys[tx_scan_info[o].in_ephemeral.pub] = m_transfers.size()-1;
-	    LOG_PRINT_L0("Received money: " << print_money(td.amount()) << ", with tx: " << txid);
+	    LOG_VERBOSE("Received money: " << print_money(td.amount()) << ", with tx: " << txid);
 	    if (0 != m_callback)
 	      m_callback->on_money_received(height, txid, tx, td.m_amount, td.m_subaddr_index, spends_one_of_ours(tx), td.m_tx.unlock_time);
           }
@@ -1108,7 +1108,7 @@ void wallet2::process_new_transaction(const crypto::hash &txid, const cryptonote
       subaddr_indices.insert(td.m_subaddr_index.minor);
       if (!pool)
       {
-        LOG_PRINT_L0("Spent money: " << print_money(amount) << ", with tx: " << txid);
+        LOG_VERBOSE("Spent money: " << print_money(amount) << ", with tx: " << txid);
         set_spent(it->second, height);
         if (0 != m_callback)
           m_callback->on_money_spent(height, txid, tx, amount, tx, td.m_subaddr_index);
