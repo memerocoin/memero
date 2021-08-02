811f12c28d0fca3529582577330ee134eb3ee475
fuwa <fuwa25519@protonmail.com>
2021-08-02 22:52:15 +0800

    lol: daemon/log: add more spacing

--- src/cryptonote/core/blockchain.cpp
+++ src/cryptonote/core/blockchain.cpp
@@ -3043,9 +3043,9 @@ leave:
      << "height:         " << new_height - 1 << std::endl
      << "difficulty:     " << current_diffic << std::endl
      << "block reward:   " << print_money(fee_summary + base_reward)
-     << config::lol::money_symbol << " (" << print_money(base_reward)
+     << config::lol::money_symbol << " ( " << print_money(base_reward)
      << config::lol::money_symbol << " + " << print_money(fee_summary)
-     << config::lol::money_symbol << ")" << std::endl
+     << config::lol::money_symbol << " )" << std::endl
      << "size:           " << cumulative_block_weight << " bytes" << std::endl
     );
   if(m_show_time_stats)
