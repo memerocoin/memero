7f9d317633b0407308c0e91eed33b78922c14ea9
fuwa <fuwa25519@protonmail.com>
2021-08-08 04:01:04 +0800

    lol: math/crypto: remove generate_ring_signature

--- src/math/crypto/crypto.hpp
+++ src/math/crypto/crypto.hpp
@@ -120,10 +120,6 @@ namespace crypto {
     friend bool check_tx_proof(const hash &, const public_key &, const public_key &, const std::optional<public_key> &, const public_key &, const signature &);
     static void generate_key_image(const public_key &, const secret_key &, key_image &);
     friend void generate_key_image(const public_key &, const secret_key &, key_image &);
-    static void generate_ring_signature(const hash &, const key_image &,
-      const public_key *const *, std::size_t, const secret_key &, std::size_t, signature *);
-    friend void generate_ring_signature(const hash &, const key_image &,
-      const public_key *const *, std::size_t, const secret_key &, std::size_t, signature *);
     static bool check_ring_signature(const hash &, const key_image &,
       const public_key *const *, std::size_t, const signature *);
     friend bool check_ring_signature(const hash &, const key_image &,
@@ -244,12 +240,6 @@ namespace crypto {
   inline void generate_key_image(const public_key &pub, const secret_key &sec, key_image &image) {
     crypto_ops::generate_key_image(pub, sec, image);
   }
-  inline void generate_ring_signature(const hash &prefix_hash, const key_image &image,
-    const public_key *const *pubs, std::size_t pubs_count,
-    const secret_key &sec, std::size_t sec_index,
-    signature *sig) {
-    crypto_ops::generate_ring_signature(prefix_hash, image, pubs, pubs_count, sec, sec_index, sig);
-  }
   inline bool check_ring_signature(const hash &prefix_hash, const key_image &image,
     const public_key *const *pubs, std::size_t pubs_count,
     const signature *sig) {
@@ -258,12 +248,7 @@ namespace crypto {
 
   /* Variants with vector<const public_key *> parameters.
    */
-  inline void generate_ring_signature(const hash &prefix_hash, const key_image &image,
-    const std::vector<const public_key *> &pubs,
-    const secret_key &sec, std::size_t sec_index,
-    signature *sig) {
-    generate_ring_signature(prefix_hash, image, pubs.data(), pubs.size(), sec, sec_index, sig);
-  }
+
   inline bool check_ring_signature(const hash &prefix_hash, const key_image &image,
     const std::vector<const public_key *> &pubs,
     const signature *sig) {
