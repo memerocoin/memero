4c8fc768eae1667d6601f432eb83d0412fddc8c0
fuwa <fuwa25519@protonmail.com>
2021-08-13 07:41:21 +0800

    lol: math/crypto: overload * for ec_scalar 5

--- src/math/crypto/crypto-ops.h
+++ src/math/crypto/crypto-ops.h
@@ -136,7 +136,7 @@ void sc_0(unsigned char *);
 /* void sc_add(unsigned char *, const unsigned char *, const unsigned char *); */
 /* void sc_sub(unsigned char *, const unsigned char *, const unsigned char *); */
 void sc_mulsub(unsigned char *, const unsigned char *, const unsigned char *, const unsigned char *);
-void sc_mul(unsigned char *, const unsigned char *, const unsigned char *);
+/* void sc_mul(unsigned char *, const unsigned char *, const unsigned char *); */
 void sc_muladd(unsigned char *s, const unsigned char *a, const unsigned char *b, const unsigned char *c);
 int sc_check(const unsigned char *);
 int sc_isnonzero(const unsigned char *); /* Doesn't normalize */
