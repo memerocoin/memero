bdf85c872f65d463cc86b1e6861cfeb9ad172c23
fuwa <fuwa25519@protonmail.com>
2021-08-29 05:40:08 +0800

    refactor/casting: bulletproofs.cpp

--- src/math/ringct/pseudo_functional/bulletproofs.cpp
+++ src/math/ringct/pseudo_functional/bulletproofs.cpp
@@ -77,7 +77,7 @@ rct::rct_point get_exponent(const rct::rct_point base, size_t idx)
 {
   constexpr std::string_view domain_separator = config::HASH_KEY_BULLETPROOF_EXPONENT;
   const std::string hashed =
-    std::string((const char*)base.data.begin(), base.data.size()) + std::string(domain_separator) + tools::get_varint_data(idx);
+    epee::string_tools::blob_to_string(base.data) + std::string(domain_separator) + tools::get_varint_data(idx);
 
   rct::rct_point e = rct::hash_to_point_via_field
     ( crypto::h2d(crypto::sha3(epee::string_tools::string_to_blob(hashed))) );
