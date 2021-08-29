63fadfb9852e014292c4013fb31a9a4cd080cd79
fuwa <fuwa25519@protonmail.com>
2021-08-29 18:33:23 +0800

    device: remove ec ops 3

--- src/cryptonote/basic/cryptonote_format_utils.cpp
+++ src/cryptonote/basic/cryptonote_format_utils.cpp
@@ -272,7 +272,7 @@ namespace cryptonote
       else
       {
         subaddr_sk = hwdev.get_subaddress_secret_key(ack.m_view_secret_key, received_index);
-        hwdev.sc_secret_add(scalar_step2, scalar_step1,subaddr_sk);
+        scalar_step2 = s2sk(scalar_step1 + subaddr_sk);
       }
 
       in_ephemeral.sec = scalar_step2;
