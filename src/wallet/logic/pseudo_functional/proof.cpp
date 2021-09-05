48bef8b1d9d4cc1d7e0ec1205d056cb9688e35d5
fuwa <fuwa25519@protonmail.com>
2021-09-05 19:04:48 +0800

    wallet/proof: fix length check on dobule schnorr

--- src/wallet/logic/pseudo_functional/proof.cpp
+++ src/wallet/logic/pseudo_functional/proof.cpp
@@ -83,7 +83,7 @@ namespace proof {
         "Signature decoding error");
       THROW_WALLET_EXCEPTION_IF(!tools::base58::decode(sig_str.substr(offset + pk_len, sig_len), sig_decoded), error::wallet_internal_error,
         "Signature decoding error");
-      THROW_WALLET_EXCEPTION_IF(sizeof(crypto::public_key) != pk_decoded.size() || sizeof(crypto::schnorr_signature) != sig_decoded.size(), error::wallet_internal_error,
+      THROW_WALLET_EXCEPTION_IF(sizeof(crypto::public_key) != pk_decoded.size() || sizeof(crypto::double_schnorr_signature) != sig_decoded.size(), error::wallet_internal_error,
         "Signature decoding error");
 
       memcpy(&shared_secret[i], pk_decoded.data(), sizeof(crypto::public_key));
