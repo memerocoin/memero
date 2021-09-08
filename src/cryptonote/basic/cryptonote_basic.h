95628c8268cdaa9567cb36843fbf19a6209a37fc
fuwa <fuwa25519@protonmail.com>
2021-09-08 09:32:20 +0800

    cn/basic: remove mutable tx/block 6

--- src/cryptonote/basic/cryptonote_basic.h
+++ src/cryptonote/basic/cryptonote_basic.h
@@ -174,22 +174,13 @@ namespace cryptonote
     void set_null();
     bool is_hash_valid() const { return false; }
     void set_hash_valid(bool v) const {}
-    bool is_prunable_hash_valid() const { return false; }
-    void set_prunable_hash_valid(bool v) const { }
+    void set_hash(const crypto::hash &h) const {}
+
     bool is_blob_size_valid() const { return false; }
     void set_blob_size_valid(bool v) const { }
-    void set_hash(const crypto::hash &h) const {}
-    void set_prunable_hash(const crypto::hash &h) const { }
     void set_blob_size(size_t sz) const { }
 
     BEGIN_SERIALIZE_OBJECT()
-      if (!typename Archive<W>::is_saving())
-      {
-        set_hash_valid(false);
-        set_prunable_hash_valid(false);
-        set_blob_size_valid(false);
-      }
-
       const unsigned int start_pos = getpos(ar);
 
       FIELDS(*static_cast<transaction_prefix *>(this))
