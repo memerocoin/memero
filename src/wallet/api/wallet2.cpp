c8adef079e013b44318181a736fd8d68248582a4
fuwa <fuwa25519@protonmail.com>
2021-09-12 14:43:51 +0800

    wallet/api: allow single output tx

--- src/wallet/api/wallet2.cpp
+++ src/wallet/api/wallet2.cpp
@@ -3227,23 +3227,7 @@ void wallet2::transfer_selected_rct(std::vector<cryptonote::tx_destination_entry
   std::vector<cryptonote::tx_destination_entry> splitted_dsts = dsts;
   cryptonote::tx_destination_entry change_dts = AUTO_VAL_INIT(change_dts);
   change_dts.amount = found_money - needed_money;
-  if (change_dts.amount == 0)
-  {
-    if (splitted_dsts.size() == 1)
-    {
-      // If the change is 0, send it to a random address, to avoid confusing
-      // the sender with a 0 amount output. We send a 0 amount in order to avoid
-      // letting the destination be able to work out which of the inputs is the
-      // real one in our rings
-      LOG_PRINT_L2("generating dummy address for 0 change");
-      cryptonote::account_base dummy;
-      dummy.generate();
-      change_dts.addr = dummy.get_keys().m_account_address;
-      LOG_PRINT_L2("generated dummy address for 0 change");
-      splitted_dsts.push_back(change_dts);
-    }
-  }
-  else
+  if (change_dts.amount != 0)
   {
     const uint32_t change_subaddress_index = subaddr_account == 0 ? 1 : 0;
     change_dts.addr = get_subaddress({subaddr_account, change_subaddress_index});
