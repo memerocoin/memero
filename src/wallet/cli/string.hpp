c98220090fa343e368c55737d1bbe3e01a2efbd2
fuwa <fuwa25519@protonmail.com>
2021-09-21 21:12:19 +0800

    wallet/cli: remove unused help messages

--- src/wallet/cli/string.hpp
+++ src/wallet/cli/string.hpp
@@ -56,7 +56,6 @@ namespace usage
                             ;
   constexpr char USAGE_SET_VARIABLE[] = "set <option> [<value>]";
   constexpr char USAGE_GET_TX_KEY[] = "get-tx-key <txid>";
-  constexpr char USAGE_VERIFY_TX_KEY[] = "verify-tx-key <txid> <txkey> <address>";
   constexpr char USAGE_GET_TX_PROOF[] = "get-tx-proof <txid> <address> [<message>]";
   constexpr char USAGE_VERIFY_TX_PROOF[] = "verify-tx-proof <txid> <address> <signature file> [<message>]";
   constexpr char USAGE_SHOW[] = "show [in|out|all|pending|failed|pool|coinbase] [index=<N1>[,<N2>,...]]\n"
@@ -69,8 +68,6 @@ namespace usage
   constexpr char USAGE_WELCOME[] = "welcome";
   constexpr char USAGE_VERSION[] = "version";
   constexpr char USAGE_HELP[] = "help [<command> | all]";
-  constexpr char USAGE_EXPORT[] = "export [in|out|all|pending|failed|pool|coinbase] [index=<N1>[,<N2>,...]]\n"
-                            "       [<min height> [<max height>]] [output=<filepath>]\n";
 } // usage
 
 namespace help {
