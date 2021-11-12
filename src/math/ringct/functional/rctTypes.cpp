f60550ffeca3face95fd5b361959b1345d801ed0
fuwa <fuwa25519@protonmail.com>
2021-11-13 00:08:07 +0800

    math/ringct: add is_bulletproof_structure_valid_extended

--- src/math/ringct/functional/rctTypes.cpp
+++ src/math/ringct/functional/rctTypes.cpp
@@ -51,6 +51,11 @@ namespace rct {
     static const size_t extra_bits = 4;
     static_assert((1 << extra_bits) == constant::BULLETPROOF_MAX_OUTPUTS, "log2(constant::BULLETPROOF_MAX_OUTPUTS) is out of date");
     LOG_ERROR_AND_RETURN_UNLESS(proof.L.size() <= 6 + extra_bits, false, "Invalid bulletproof L size");
+
+    return true;
+  }
+
+  bool is_bulletproof_structure_valid_extended(const Bulletproof_unsafe &proof) {
     LOG_ERROR_AND_RETURN_UNLESS(proof.V.size() <= (1u<<(proof.L.size()-6)), false, "Invalid bulletproof V/L");
     LOG_ERROR_AND_RETURN_UNLESS(proof.V.size() * 2 > (1u<<(proof.L.size()-6)), false, "Invalid bulletproof V/L");
     LOG_ERROR_AND_RETURN_UNLESS(proof.V.size() > 0, false, "Empty bulletproof");
@@ -61,6 +66,7 @@ namespace rct {
   size_t n_bulletproof_amounts(const Bulletproof_unsafe &proof)
   {
     LOG_ERROR_AND_RETURN_UNLESS(is_bulletproof_structure_valid(proof), 0, "Invalid proof structure");
+    LOG_ERROR_AND_RETURN_UNLESS(is_bulletproof_structure_valid_extended(proof), 0, "Invalid proof structure");
     return proof.V.size();
   }
 
@@ -80,11 +86,7 @@ namespace rct {
 
   size_t n_bulletproof_max_amounts(const Bulletproof_unsafe &proof)
   {
-    LOG_ERROR_AND_RETURN_UNLESS(proof.L.size() >= 6, 0, "Invalid bulletproof L size");
-    LOG_ERROR_AND_RETURN_UNLESS(proof.L.size() == proof.R.size(), 0, "Mismatched bulletproof L/R size");
-    static const size_t extra_bits = 4;
-    static_assert((1 << extra_bits) == constant::BULLETPROOF_MAX_OUTPUTS, "log2(constant::BULLETPROOF_MAX_OUTPUTS) is out of date");
-    LOG_ERROR_AND_RETURN_UNLESS(proof.L.size() <= 6 + extra_bits, 0, "Invalid bulletproof L size");
+    LOG_ERROR_AND_RETURN_UNLESS(is_bulletproof_structure_valid(proof), 0, "Invalid proof structure");
     return 1 << (proof.L.size() - 6);
   }
 
