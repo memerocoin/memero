697b85f164aded8dd10ed2c415de70efc6ab68ef
fuwa <fuwa25519@protonmail.com>
2021-11-12 13:23:27 +0800

    math/bp: do not expose interface that uses raw scalars as amount

--- src/math/ringct/pseudo_functional/bulletproofs.hpp
+++ src/math/ringct/pseudo_functional/bulletproofs.hpp
@@ -38,7 +38,6 @@
 namespace rct
 {
 
-Bulletproof bulletproof_MAKE(const rct::rct_scalarV v, const rct::rct_scalarV gamma);
 Bulletproof bulletproof_MAKE(const std::vector<uint64_t> v, const rct::rct_scalarV gamma);
 bool bulletproof_VERIFY(const Bulletproof proof);
 
