6b695ab3afb0fafa08e6043d87f3b0f868ad71fb
fuwa <fuwa25519@protonmail.com>
2021-11-12 21:59:15 +0800

    math/ringct: use sizeof in hash_and_xor_int

--- src/math/ringct/functional/rctOps.cpp
+++ src/math/ringct/functional/rctOps.cpp
@@ -122,7 +122,7 @@ namespace rct {
     const crypto::hash h = derive_secret_key_for_ecdh_amount(y);
     crypto::ec_scalar r = crypto::int_to_scalar(x);
 
-    for (int i = 0; i < 8; ++i)
+    for (int i = 0; i < sizeof(uint64_t); ++i)
       r.data[i] ^= h.data[i];
 
     return crypto::scalar_to_int(r);
