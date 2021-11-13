aee37b125781176a0dc1030b7b737efe35413f34
fuwa <fuwa25519@protonmail.com>
2021-11-13 13:41:27 +0800

    math/bp: make k a constant

--- src/math/ringct/pseudo_functional/bulletproofs.cpp
+++ src/math/ringct/pseudo_functional/bulletproofs.cpp
@@ -725,7 +725,6 @@ bool bulletproof_VERIFY(const Bulletproof proof)
 
   // collect
   const rct::rct_scalar ip1y = vector_power_sum(pd.y, MN);
-  rct::rct_scalar k = s_zero - zpow[2] * ip1y;
   LOG_ERROR_AND_RETURN_UNLESS(M+2 < zpow.size(), false, "invalid zpow index");
 
   const auto zpow_it = std::next(zpow.begin(), 3);
@@ -737,8 +736,7 @@ bool bulletproof_VERIFY(const Bulletproof proof)
      , s_zero
      );
 
-  k = k - k1 * ip12;
-
+  const rct_scalar k = s_zero - zpow[2] * ip1y - k1 * ip12;
 
   const rct_scalar y0 = s_zero - proof.taux * weight_y;
   const rct_scalar y1 = (proof.t - (pd.z * ip1y + k)) * weight_y;
