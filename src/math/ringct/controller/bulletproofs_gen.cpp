676fd9ce531f1a53681bb2bb71901a52a3458953
fuwa <fuwa25519@protonmail.com>
2021-11-21 21:16:58 +0800

    math/bp: tweak error message

--- src/math/ringct/controller/bulletproofs_gen.cpp
+++ src/math/ringct/controller/bulletproofs_gen.cpp
@@ -77,7 +77,7 @@ rct_point get_exponent(const rct_point base, size_t idx)
   rct_point e = crypto::hash_to_point_via_field
     ( crypto::h2d(crypto::sha3(epee::string_tools::string_to_blob(hashed))) );
 
-  LOG_ERROR_AND_THROW_IF((e == crypto::identity), "Exponent is point at infinity");
+  LOG_ERROR_AND_THROW_IF((e == crypto::identity), "Invalid exponent");
   return e;
 }
 
