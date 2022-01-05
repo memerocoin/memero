30aa5ca56c4bdb2ca3c54d89238086e73eded9cd
fuwa <fuwa25519@protonmail.com>
2022-01-05 14:46:09 +0800

    release: v0.9.9.83

--- src/config/version/version.cpp
+++ src/config/version/version.cpp
@@ -1,4 +1,4 @@
 #include "version.hpp"
 
-constexpr std::string_view LOLNERO_VERSION = "0.9.9.82";
+constexpr std::string_view LOLNERO_VERSION = "0.9.9.83";
 constexpr std::string_view LOLNERO_RELEASE_NAME = "Saber";
