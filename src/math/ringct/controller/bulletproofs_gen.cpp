e90f04ff105def4679b1002f9907abe657f881ea
fuwa <fuwa25519@protonmail.com>
2022-01-21 18:59:34 +0800

    math/bp: make rewritten bp_gen gpl3

--- src/math/ringct/controller/bulletproofs_gen.cpp
+++ src/math/ringct/controller/bulletproofs_gen.cpp
@@ -18,38 +18,6 @@
 */
 
 
-// Copyright (c) 2021, The Lolnero Project
-// Copyright (c) 2017-2020, The Monero Project
-//
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without modification, are
-// permitted provided that the following conditions are met:
-//
-// 1. Redistributions of source code must retain the above copyright notice, this list of
-//    conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright notice, this list
-//    of conditions and the following disclaimer in the documentation and/or other
-//    materials provided with the distribution.
-//
-// 3. Neither the name of the copyright holder nor the names of its contributors may be
-//    used to endorse or promote products derived from this software without specific
-//    prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
-// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// Adapted from Java code by Sarang Noether
-// Paper references are to https://eprint.iacr.org/2017/1066 (revision 1 July 2018)
-
 #include "bulletproofs_gen.hpp"
 
 #include "math/ringct/functional/vectorOps.hpp"
@@ -134,6 +102,27 @@ namespace rct
     }
   }
 
+  scalarV int_to_bits(const uint64_t x) {
+    scalarV xs;
+
+    std::generate_n
+      (
+       std::back_inserter(xs)
+       , bit_width
+       , [x, i = 1ull] () mutable {
+         const auto bit =
+           (x & i) > 0
+           ? crypto::s_1
+           : crypto::s_0;
+
+         i = i << 1;
+
+         return bit;
+       }
+       );
+    return xs;
+  }
+
 
   crypto::ec_point commit_vectors_with_bp_generators_G_H
   (
@@ -176,26 +165,30 @@ namespace rct
        }
        );
 
-    scalarV aL(total_bit_width), aR(total_bit_width);
+    // scalarV aL3(total_bit_width);
+    // scalarV aR(total_bit_width);
+
     // PAPER LINES 41-42
-    for (size_t j = 0; j < padded_number_of_inputs; ++j)
-      {
-        for (size_t i = bit_width; i-- > 0; )
-          {
-            const crypto::ec_scalar amount_scalar = crypto::int_to_scalar(xs[j].first);
-            const size_t idx = j * bit_width + i;
-            if (j < xs.size() && (amount_scalar.data[i/8] & (((uint64_t)1)<<(i%8))))
-              {
-                aL[idx] = rct::s_one;
-                aR[idx] = rct::s_zero;
-              }
-            else
-              {
-                aL[idx] = rct::s_zero;
-                aR[idx] = rct::s_minus_one;
-              }
-          }
-      }
+    std::vector<scalarV> bits;
+    std::transform
+      (
+       xs.begin()
+       , xs.end()
+       , std::back_inserter(bits)
+       , [](const auto& x) -> scalarV {
+         return int_to_bits(x.first);
+       }
+       );
+
+    std::generate_n
+      (
+       std::back_inserter(bits)
+       , padded_number_of_inputs - xs.size()
+       , []() { return vector_repeat(crypto::s_0, bit_width); }
+       );
+
+    const scalarV aL = vector_concat(bits);
+    const scalarV aR = vector_subtract(aL, crypto::s_1);
 
   try_again:
     // PAPER LINES 43-44
