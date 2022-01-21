72a235dde66ee069f65a80416b905c0e18bfa567
fuwa <fuwa25519@protonmail.com>
2022-01-22 01:46:16 +0800

    math/bp: add code ref back to the original code

--- src/math/ringct/controller/bulletproofs_gen.cpp
+++ src/math/ringct/controller/bulletproofs_gen.cpp
@@ -15,6 +15,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+Adapted from C++ code from The Monero Project
+Adapted from Java code by Sarang Noether
+Paper references are to https://eprint.iacr.org/2017/1066
+(revision 1 July 2018)
+
 */
 
 
