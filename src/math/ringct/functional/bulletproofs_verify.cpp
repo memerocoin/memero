7e04fa1191456699cfa00512399b8fce31ea9ffd
fuwa <fuwa25519@protonmail.com>
2022-01-26 04:05:54 +0800

    math/bp: layout verify

--- src/math/ringct/functional/bulletproofs_verify.cpp
+++ src/math/ringct/functional/bulletproofs_verify.cpp
@@ -247,24 +247,6 @@ namespace rct
     const size_t total_bit_width =
       padded_number_of_inputs * bit_width;
 
-    const auto challenge_y = challenges.V_A_S;
-
-    const auto y_exponents =
-      scalar_exponents(challenge_y, total_bit_width);
-
-    const scalarV two_exponents =
-      scalar_exponents(rct::s_two, bit_width);
-
-    const std::vector<scalarV> z_exp_mult_two_exp_monadic =
-      vector_mult_V_monadic
-      (
-       std::span(z_exponents).subspan(2, padded_number_of_inputs)
-       , two_exponents
-       );
-
-    const scalarV z_exp_mult_two_exp_flatten =
-      vector_concat(z_exp_mult_two_exp_monadic);
-
     scalarV w_cache(total_bit_width);
     w_cache[0] = w_inv_V[0];
     w_cache[1] = challenges.inner_product_LR_challenges[0];
@@ -307,6 +289,11 @@ namespace rct
     auto w_cache_reverse = w_cache;
     std::reverse(w_cache_reverse.begin(), w_cache_reverse.end());
 
+    const auto challenge_y = challenges.V_A_S;
+
+    const auto y_exponents =
+      scalar_exponents(challenge_y, total_bit_width);
+
     const crypto::ec_scalar y_inv =
       crypto::multiplicative_inverse(challenge_y);
 
@@ -323,6 +310,19 @@ namespace rct
         )
        , proof.b
        );
+
+    const scalarV two_exponents =
+      scalar_exponents(rct::s_two, bit_width);
+
+    const std::vector<scalarV> z_exp_mult_two_exp_monadic =
+      vector_mult_V_monadic
+      (
+       std::span(z_exponents).subspan(2, padded_number_of_inputs)
+       , two_exponents
+       );
+
+    const scalarV z_exp_mult_two_exp_flatten =
+      vector_concat(z_exp_mult_two_exp_monadic);
 
     const auto z5_h2 =
       vector_add_V
