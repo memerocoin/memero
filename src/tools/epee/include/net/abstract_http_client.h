3e182ea82dbcf5c95fd805cf3cbe92ef5bcbb5cd
fuwa <fuwa25519@protonmail.com>
2022-02-17 22:35:48 +0800

    epee: remove abstract_http_client

--- src/tools/epee/include/net/abstract_http_client.h
+++ src/tools/epee/include/net/abstract_http_client.h
@@ -31,25 +31,6 @@ namespace epee
 {
 namespace net_utils
 {
-  inline const char* get_hex_vals()
-  {
-    static constexpr const char hexVals[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-    return hexVals;
-  }
-
-  inline const char* get_unsave_chars()
-  {
-    //static constexpr char unsave_chars[] = "\"<>%\\^[]`+$,@:;/!#?=&";
-    static constexpr const char unsave_chars[] = "\"<>%\\^[]`+$,@:;!#&";
-    return unsave_chars;
-  }
-
-  bool is_unsafe(unsigned char compare_char);
-  std::string dec_to_hex(char num, int radix);
-  int get_index(const char *s, char c);
-  std::string convert(char val);
-  std::string conver_to_url_format(const std::string& uri);
-  std::string convert_from_url_format(const std::string& uri);
 
 }
 }
