9c732dc095b3e996d2e9b424e5d9fa346121d582
fuwa <fuwa25519@protonmail.com>
2022-03-03 19:43:36 +0800

    wallet/cli: remove split_amounts

--- src/wallet/cli/simplewallet.h
+++ src/wallet/cli/simplewallet.h
@@ -120,9 +120,6 @@ namespace cryptonote
     bool show_incoming(const std::vector<std::string> &args);
     bool transfer_main(int transfer_type, const std::vector<std::string> &args);
     bool transfer(const std::vector<std::string> &args);
-    std::vector<std::vector<cryptonote::tx_destination_entry>> split_amounts(
-        std::vector<cryptonote::tx_destination_entry> dsts, size_t num_splits
-    );
     bool account(const std::vector<std::string> &args = std::vector<std::string>());
     void print_accounts();
     bool print_address(const std::vector<std::string> &args = std::vector<std::string>());
